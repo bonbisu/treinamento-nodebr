f95b1166e9ebeb82e52b0e37d57fb5619738aadb
bonbisu <fernando.costa30@fatec.sp.gov.br>
2019-03-12 16:33:43 -0300

    :up: update(functions): create all async handle functions
    working, but too many nested functions

--- 02-nodejs/index.js
+++ 02-nodejs/index.js
@@ -5,6 +5,9 @@
 */
 
 function obterUsuario(callback) {
+  // o callback é apenas o padrao function (erro, sucesso)
+  // não importando o nome da mesma, podendo ser anonima(arrow)
+  // o callback, por padrão é o ultimo parametro
   setTimeout(function() {
     return callback(null, {
       // retorna usuario para obterUsuario após 1 segundo
@@ -15,22 +18,60 @@ function obterUsuario(callback) {
   }, 1000);
 }
 
-function obterTelefone(idUsuario) {
+function obterTelefone(idUsuario, callback) {
   setTimeout(() => {
-    return {
+    return callback(null, {
       telefone: '99999-9999',
       ddd: 11
-    };
+    });
   }, 2000);
 }
 
-function obterEndereco(idUsuario) {}
+function obterEndereco(idUsuario, callbackson) {
+  setTimeout(() => {
+    return callbackson(null, {
+      rua: 'dos bobos',
+      numero: 0
+    });
+  }, 2000);
+}
 
 function resolverUsuario(erro, usuario) {
   console.log('usuario', usuario);
 }
 
-obterUsuario(resolverUsuario); // passando uma função como callback
+obterUsuario(function resolverUsuario(error, usuario) {
+  // null || "" || 0 === false
+  if (error) {
+    console.error('DEU RUIM em USUARIO', error);
+    return;
+  }
+  obterTelefone(usuario.id, function resolverTelefone(error1, telefone) {
+    if (error1) {
+      console.error('DEU RUIM em TELEFONE', error1);
+      return;
+    }
+    obterEndereco(usuario.id, (error2, endereco) => {
+      if (error2) {
+        console.error('DEU RUIM em ENDERECO', error2);
+      }
+      // usando um string dinamica com variaveis
+      console.log(`
+      Nome: ${usuario.nome},
+      Endereco: ${endereco.rua}, ${endereco.numero}
+      Telefone: (${telefone.ddd}) ${telefone.telefone}
+      `);
+      console.log(
+        // devolvendo tbm como json
+        JSON.stringify({
+          nome: usuario.nome,
+          endereco: endereco.rua + ', ' + endereco.numero,
+          telefone: '(' + telefone.ddd + ') ' + telefone.telefone
+        })
+      );
+    });
+  });
+}); // passando uma função como callback
 
 // const telefone = obterTelefone(usuario.id);
 
